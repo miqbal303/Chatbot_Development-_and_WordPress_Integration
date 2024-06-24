d77445de4fd7a8681fcd0f9803cdc5f3d2cfe9cf
miqbal303 <miqbal303@gmail.com>
2024-06-24 19:09:32 +0530

    "update"

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app.config import configure_app
-from app.routes import register_routes
-
-def create_app():
-    app = Flask(__name__)
-    configure_app(app)
-    register_routes(app)
-    return app
