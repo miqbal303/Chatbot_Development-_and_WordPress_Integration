4ffe58a0d3bcaa1398448e9203cc724e2ef2033f
miqbal303 <miqbal303@gmail.com>
2024-06-24 21:04:09 +0530

    update model

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 def configure_app(app):
     app.config['MODEL_ID'] = os.getenv('MODEL_ID', 'sentence-transformers/all-MiniLM-L6-v2')
-    app.config['LLM_MODEL_ID'] = os.getenv('LLM_MODEL_ID', 'distilgpt2')  # This is for text generation
+    app.config['LLM_MODEL_ID'] = os.getenv('LLM_MODEL_ID', 'EleutherAI/gpt-neo-125M')  # This is for text generation
     try:
         app.config['EMBEDDING_DIM'] = int(os.getenv('EMBEDDING_DIM', 384))
     except ValueError:
